1adb987950b04fe72e95000a4d588c8d3f27e55c
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2015-06-04 20:21:24 +0200

    qtrle: allow saving video file

--- qtrle.py
+++ qtrle.py
@@ -12,9 +12,12 @@ import mp4select
 
 # http://wiki.multimedia.cx/index.php?title=Apple_QuickTime_RLE
 
+cv2.namedWindow("display", cv2.WINDOW_NORMAL)
+
 def redraw(frame):
-	cv2.imshow("display", cv2.pyrDown(frame[:,:,::-1]))
-	cv2.imshow("alpha", cv2.pyrDown(frame[:,:,0]))
+	cv2.imshow("display", (frame[:,:,::-1]))
+	#cv2.imshow("display", cv2.pyrDown(frame[:,:,::-1]))
+	#cv2.imshow("alpha", cv2.pyrDown(frame[:,:,0]))
 	while True:
 		key = cv2.waitKey(1)
 		if key == -1:
@@ -24,6 +27,7 @@ def redraw(frame):
 
 
 def decode_chunk(frame, chunk):
+	xmax = ymax = 0
 	header = (chunk >> ">H")
 
 	#print "size, header:", len(chunk)+4, header
@@ -78,6 +82,7 @@ def decode_chunk(frame, chunk):
 
 			elif rlecode == -1:
 				py += 1
+				ymax = max(ymax, py)
 				px = 0
 				
 				linecount += 1
@@ -94,11 +99,13 @@ def decode_chunk(frame, chunk):
 				pixels = [(chunk >> ">BBBB") for i in xrange(rlecode)]
 				frame[py,px : px + rlecode] = pixels
 				px += rlecode
+				xmax = max(xmax, px)
 
 			elif rlecode < -1:
 				pixel = (chunk >> ">BBBB")
 				frame[py, px : px-rlecode] = pixel
 				px += -rlecode
+				xmax = max(xmax, px)
 
 
 		else:
@@ -109,18 +116,24 @@ def decode_chunk(frame, chunk):
 	if firstline == 0 and (linecount == frame.shape[0]) and not skipused:
 		print "that was a KEYFRAME"
 	
+	return (xmax, ymax)
 
 
 
 
 if __name__ == '__main__':
-	(fname,) = sys.argv[1:]
+	width, height = sys.argv[1:3]
+	width = int(width) or 4096
+	height = int(height) or 2048
 
-	buf = FileBuffer(fname)
+	infname = sys.argv[3]
+
+	outvid = (len(sys.argv[1:]) > 3) and sys.argv[4]
+
+	buf = FileBuffer(infname)
 	
 	(mdat,) = mp4select.select("mdat", buf)
 	
-	width, height = 1440, 900
 	frame = np.zeros((height, width, 4), dtype=np.uint8)
 	
 	# iterate chunks
@@ -132,6 +145,20 @@ if __name__ == '__main__':
 		mdat.pos += chunksize-4
 
 		print "decoding frame", framecount		
-		decode_chunk(frame, chunk)
+		(mx, my) = decode_chunk(frame, chunk)
+
+		# detect resolution
+		if framecount == 0:
+			print "detected resolution {0} x {1}".format(mx, my)
+			assert mx <= width and my <= height
+			(width, height) = (mx, my)
+			frame = frame[:height, :width]
+
+			if outvid is not False:
+				outvid = cv2.VideoWriter(outvid, -1, 25, (width, height))
+				assert outvid.isOpened()
+
+		if outvid:
+			outvid.write(frame)
 
 		framecount += 1
